084c04c237ee74faf39821a6261c4df9011b45bb
its-kumar-yash <its.yash.kumar23@gmail.com>
2023-06-16 09:48:49 +0530

    fetched data from the api

--- src/components/QuoteCard.jsx
+++ src/components/QuoteCard.jsx
@@ -1,9 +1,27 @@
-import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useEffect, useState } from "react";
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { fas } from '@fortawesome/free-solid-svg-icons'
+// import { faTwitter, faFontAwesome } from '@fortawesome/free-brands-svg-icons'
+// import { faSearch } from "@fortawesome/free-solid-svg-icons";
+// library.add(fas, faTwitter, faFontAwesome)
 
 const QuoteCard = () => {
   const [quotes, setQuotes] = useState([]);
   const [randomQuote, setRandomQuote] = useState("");
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch("https://type.fit/api/quotes");
+            const data = await response.json();
+            // console.log(data);
+            setQuotes(data);
+            let randIdx = Math.floor(Math.random() * data.length);
+            setRandomQuote(data[randIdx]);
+            // console.log(data[randIdx]);
+        }
+        fetchData();
+    }, []);
+
 
   return (
     <div className="card">
@@ -14,14 +32,12 @@ const QuoteCard = () => {
         <div className="flex">
           <div className="child1">
             <a>
-              <FontAwesomeIcon icon={["fab", "apple"]} />
               <i className="fab fa-twitter"></i>
             </a>
           </div>
           <div className="child2">
             <a>
-              <FontAwesomeIcon icon={["fab", "facebook-f"]} />
-              <FontAwesomeIcon icon={["fab", "twitter"]} />
+            {/* <FontAwesomeIcon icon="fa-solid fa-twitter" /> */}
             </a>
           </div>
           <div className="child3">
